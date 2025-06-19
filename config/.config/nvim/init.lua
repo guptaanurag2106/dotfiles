824797911df29be3ce7774ff9bae92d384749ad9
guptaanurag2106 <2106.guptaanurag@gmail.com>
2025-06-19 23:15:54 +0530

    Add wlsunset

--- config/.config/nvim/init.lua
+++ config/.config/nvim/init.lua
@@ -1,1 +1,1 @@
-    require("anurag")
+require("anurag")
--- config/.config/scripts/wl-sunset.sh
+++ config/.config/scripts/wl-sunset.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
+wlsunset -l 12.9 -L 77.6 &
+
 while true; do
-    swaymsg -t subscribe '["window"]' | jq 'select(.change=="focus" or .change=="fullscreen_mode").container | if (.app_id == "org.mozilla.firefox" or .app_id == "microsoft-edge") then halt_error(127 - .fullscreen_mode) else halt end' > /dev/null 2>&1
+    swaymsg -t subscribe '["window"]' | jq 'select(.change).container | if (.app_id == "org.mozilla.firefox" or .app_id == "microsoft-edge") then halt_error(127 - .fullscreen_mode) else halt end' > /dev/null 2>&1
     return_code=$?
     echo "$return_code"
     if [ $return_code -eq 126 ]; then
