0c5b5aacbaad7c62516d3dadd722da223b73a45d
guptaanurag2106 <2106.guptaanurag@gmail.com>
2026-02-15 05:54:55 +0530

    Fix completion, lsp and pdf-tools

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,4 +1,4 @@
-;;;; init.el My Emacs config
+;;; init.el --- Init  -*- lexical-binding: t; -*-
 
 ;; Init, Core
 (fset 'yes-or-no-p 'y-or-n-p)
@@ -14,7 +14,7 @@
 (save-place-mode 1) ;; remember cursor positions in files
 
 (recentf-mode 1) ;; track recently opened files
-(setq recentf-max-saved-items 200)
+(setq recentf-max-saved-items 50)
 
 (winner-mode 1) ;; undo window layout changes
 
@@ -27,23 +27,24 @@
 
 (setq create-lockfiles nil) ;; do not create lockfiles (#file)
 
-;; backups in one directory, versioned
+;; Set backup and autosave options
+(make-directory (expand-file-name "autosave/" user-emacs-directory) t)
 (setq make-backup-files t
       version-control t
       backup-by-copying t
       delete-old-versions t
       kept-new-versions 6
       kept-old-versions 2
+      auto-save-file-name-transforms
+      `((".*" ,(expand-file-name "autosave/" user-emacs-directory) t))
       backup-directory-alist
       `(("." . ,(expand-file-name "backup/" user-emacs-directory))))
-
-(modify-coding-system-alist 'file "" 'utf-8) ;; default UTF-8 handling
 
 ;; browser integration
 (setq browse-url-browser-function 'browse-url-generic
       browse-url-generic-program "firefox")
 
-(setq confirm-kill-emacs 'y-or-n-p) ;; confirm before quitting, bad :q habit
+;; (setq confirm-kill-emacs 'y-or-n-p) ;; confirm before quitting, bad :q habit
 
 (setq sentence-end-double-space nil) ;; sentence detection: single space
 
@@ -53,7 +54,7 @@
       '(("elpa"  . "https://elpa.gnu.org/packages/")
         ("melpa" . "https://melpa.org/packages/")))
 
-(package-initialize)
+(package-initialize) ;; not needed after emacs 27
 
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
@@ -67,9 +68,11 @@
 (setq show-paren-delay 0.1)
 
 ;; theme + font
-(add-to-list 'custom-theme-load-path "~/.emacs.d/themes") ;; TODO: relative path
+(add-to-list 'custom-theme-load-path
+     (expand-file-name "themes" user-emacs-directory))
 (load-theme 'gruber-darker t)
 (set-frame-font "Inconsolata Nerd Font Mono 15" nil t)
+(setq frame-inhibit-implied-resize t) ;; useless for a tiling window manager
 
 ;; line & column numbers
 (setq display-line-numbers-type 'relative)
@@ -104,7 +107,7 @@
 
 ;;; Buffer placement rules
 (setq display-buffer-alist
-      '(("\\*\\(compilation\\|Warnings\\|Help\\|Messages\\|xref\\|eldoc\\).*\\*"
+      '(("\\*\\(compilation\\|Warnings\\|Help\\|Messages\\|xref\\|eldoc\\)\\*"
          (display-buffer-reuse-window display-buffer-at-bottom)
          (window-height . 0.20))))
 
@@ -114,6 +117,7 @@
       dired-create-destination-dirs 'ask
       dired-dwim-target t
       trash-directory "~/.local/share/Trash/files")
+(put 'dired-find-alternate-file 'disabled nil)
 
 ;;; Local PATH additions
 (dolist (p '("~/go/bin" "~/opt/gf" "~/.local/bin/"))
@@ -123,19 +127,22 @@
       (setenv "PATH"
               (concat dir path-separator (getenv "PATH"))))))
 
-;;; Optional compatibility
-(cua-mode) ;; enables CUA copy/paste keys while keeping Emacs behavior
+;; (cua-mode) ;; enables CUA copy/paste keys while keeping Emacs behavior
 
 ;;; Evil
+;; (setq evil-respect-visual-line-mode t) ;; respect visual lines
+;; (setq evil-search-module 'isearch) ;; use emacs' built-in search functionality.
+;; (setq evil-want-integration t) ;; necessary for evil collection
+;; (setq evil-want-C-i-jump nil) ;; hopefully this will fix weird tab behaviour
 ;; must be set BEFORE evil loads
 (setq evil-want-keybinding nil      ; required for evil-collection
       evil-want-symbol-word t       ; treat symbols as words
       evil-want-minibuffer t        ; enable evil in minibuffer
-      evil-kill-on-visual-paste nil)
+      evil-kill-on-visual-paste nil
+      evil-undo-system 'undo-redo)
 
 (use-package evil
      :init
-     :config
      (evil-mode 1))
 
 (use-package evil-collection
@@ -143,36 +150,16 @@
      :config
      (evil-collection-init))
 
+;; Vim style number inc/dec
+(use-package evil-numbers
+     :after evil
+     :commands (evil-numbers/inc-at-pt
+                     evil-numbers/dec-at-pt))
+
 ;; Search behaviour
 (setq isearch-lazy-count t
       search-upper-case t
       isearch-case-fold-search nil)
-
-;; Common key tweaks
-(with-eval-after-load 'evil
-      ;; gcc / gc comments using built-in functions
-      (evil-define-key 'normal 'global (kbd "gcc") #'comment-line)
-      (evil-define-key 'visual 'global (kbd "gc")  #'comment-region)
-
-      ;; center after search like vim nzzv
-      (evil-define-key 'normal 'global
-            (kbd "n")
-            (lambda ()
-              (interactive)
-              (evil-search-next)
-              (evil-scroll-line-to-center nil)
-              (evil-show-jump)))
-
-      ;; visual paste without replacing register
-      (evil-define-key 'visual 'global
-            (kbd "SPC p") #'evil-paste-before)
-
-      ;; diagnostics navigation
-      (evil-define-key 'normal 'global
-            (kbd "]d") #'flymake-goto-next-error
-            (kbd "[d") #'flymake-goto-prev-error
-            (kbd "]c") #'next-error
-            (kbd "[c") #'previous-error))
 
 ;; Move selected lines
 (defun evil-move-visual-lines (direction)
@@ -198,8 +185,52 @@
        (interactive)
        (evil-move-visual-lines -1))
 
-(define-key evil-visual-state-map (kbd "J") #'evil-move-visual-down)
-(define-key evil-visual-state-map (kbd "K") #'evil-move-visual-up)
+(defun my-split-vertical ()
+       (interactive)
+       (split-window-vertically)
+       (other-window 1))
+
+(defun my-split-horizontal ()
+       (interactive)
+       (split-window-horizontally)
+       (other-window 1))
+
+;; Common key tweaks
+(with-eval-after-load 'evil
+      ;; gcc / gc comments using built-in functions
+      (evil-define-key 'normal 'global (kbd "gcc") #'comment-line)
+      (evil-define-key 'visual 'global (kbd "gc") #'comment-or-uncomment-region)
+      (evil-define-key 'normal 'global (kbd "g+") #'evil-numbers/inc-at-pt)
+      (evil-define-key 'normal 'global (kbd "g-") #'evil-numbers/dec-at-pt)
+
+
+      ;; center after search like vim nzzv
+      (evil-define-key 'normal 'global
+            (kbd "n")
+            (lambda ()
+              (interactive)
+              (evil-search-next)
+              (evil-scroll-line-to-center)
+              (evil-show-jump)))
+
+      ;; visual paste without replacing register
+      (evil-define-key 'visual 'global
+            (kbd "SPC p") #'evil-paste-before)
+
+      ;; diagnostics navigation
+      (evil-define-key 'normal 'global
+            (kbd "]d") #'flymake-goto-next-error
+            (kbd "[d") #'flymake-goto-prev-error
+            (kbd "]c") #'next-error
+            (kbd "[c") #'previous-error)
+
+
+      (define-key evil-normal-state-map (kbd "C-w s") #'my-split-vertical)
+      (define-key evil-normal-state-map (kbd "C-w v") #'my-split-horizontal)
+      (define-key evil-visual-state-map (kbd "J") #'evil-move-visual-down)
+      (define-key evil-visual-state-map (kbd "K") #'evil-move-visual-up)
+      (define-key evil-normal-state-map (kbd "C-c c") #'compile)
+      (define-key evil-normal-state-map (kbd "-") #'dired-jump))
 
 ;; Syntax tweaks
 (defun my/prog-syntax-setup ()
@@ -210,6 +241,19 @@
 (add-hook 'prog-mode-hook #'my/prog-syntax-setup)
 
 ;; Leader key
+(require 'project)
+
+(defun my/format-buffer () ;; run ruff for python files as baseedpyright doesn't support formatting
+       (interactive)
+       (cond
+        ((eq major-mode 'python-mode)
+         (when buffer-file-name
+               (call-process "ruff" nil "*ruff*" nil
+                     "format" buffer-file-name)
+               (revert-buffer t t t)))
+        ((eglot-current-server)
+         (eglot-format))))
+
 (use-package general
      :after evil
      :config
@@ -219,16 +263,16 @@
               :prefix "SPC")
 
      (my/leader
+       "p" project-prefix-map
        "SPC" 'recentf
        "s f" 'find-file
-       "-"   'dired
        "b b" 'consult-buffer
        "b d" 'kill-current-buffer
        "g s" 'magit-status
        "g g" 'magit-dispatch
        "r n" 'eglot-rename
        "c a" 'eglot-code-actions
-       "f d" 'eglot-format
+       "f d" #'my/format-buffer
        "t h" 'eglot-inlay-hints-mode
        "g d" 'xref-find-definitions
        "g D" 'eglot-find-declaration
@@ -237,20 +281,13 @@
        "r r" 'xref-find-references
        "v d" 'flymake-show-buffer-diagnostics))
 
-
-;; Which key
 (use-package which-key
      :defer 1
-     :config
+     :init
      (which-key-mode 1))
 
 ;; Compilation
-(define-key evil-normal-state-map (kbd "C-c c") #'compile)
 (setq compilation-read-command t)
-
-;; Vim style number inc/dec
-(use-package evil-numbers
-     :commands (evil-numbers/inc-at-pt evil-numbers/dec-at-pt))
 
 ;;; Minibuffer completion
 (use-package vertico
@@ -289,16 +326,15 @@
 
 ;;; In-buffer completion
 (use-package corfu
-     :init
-     (add-hook 'prog-mode-hook #'corfu-mode)
      :custom
-     (corfu-auto t)
+     (corfu-auto nil)
      (corfu-auto-prefix 2)
      (corfu-auto-delay 0.1)
      (corfu-preview-current nil)
      (corfu-preselect nil)
      (completion-cycle-threshold 3)
      :config
+     (global-corfu-mode 1)
      ;; TAB completes
      (define-key corfu-map (kbd "TAB") #'corfu-complete)
 
@@ -315,23 +351,28 @@
 (with-eval-after-load 'corfu
       (require 'corfu-popupinfo)
       (corfu-popupinfo-mode 1)
-      (setq corfu-popupinfo-delay 0.3))
+      (setq corfu-popupinfo-delay 0.2))
 
 ;;; Extra completion sources
 (use-package cape
      :init
-     ;; make eglot completion composable
      (advice-add 'eglot-completion-at-point
              :around #'cape-wrap-buster)
 
-     ;; fallback sources
-     (add-to-list 'completion-at-point-functions #'cape-file)
-     (add-to-list 'completion-at-point-functions #'cape-dabbrev)
-     (add-to-list 'completion-at-point-functions #'cape-keyword))
+     ;; global fallback completion
+     (add-to-list 'completion-at-point-functions
+          (cape-capf-super
+                #'cape-file
+                #'cape-dabbrev)))
 
 ;;; Snippets
-(use-package yasnippet
-     :hook (prog-mode . yas-minor-mode))
+;; (use-package yasnippet
+;;      :config
+;;      (yas-global-mode 1))
+;; (use-package yasnippet-snippets)
+;; (with-eval-after-load 'cape
+;;       (add-to-list 'completion-at-point-functions
+;;            #'cape-yasnippet))
 
 ;;; Tree-sitter
 (setq treesit-language-source-alist
@@ -342,12 +383,19 @@
 
 (setq treesit-font-lock-level 4)
 
-(add-to-list 'major-mode-remap-alist '(go-mode . go-ts-mode))
-(add-to-list 'major-mode-remap-alist '(c-mode . c-ts-mode))
-(add-to-list 'major-mode-remap-alist '(c++-mode . c++-ts-mode))
-(add-to-list 'major-mode-remap-alist '(lua-mode . lua-ts-mode))
-(add-to-list 'auto-mode-alist '("\\.go\\'" . go-ts-mode))
-(add-to-list 'auto-mode-alist '("\\.lua\\'" . lua-ts-mode))
+(when (treesit-available-p)
+  (add-to-list 'major-mode-remap-alist '(go-mode . go-ts-mode))
+  (add-to-list 'major-mode-remap-alist '(c-mode . c-ts-mode))
+  (add-to-list 'major-mode-remap-alist '(c++-mode . c++-ts-mode))
+  (add-to-list 'major-mode-remap-alist '(lua-mode . lua-ts-mode))
+  (add-to-list 'auto-mode-alist '("\\.go\\'" . go-ts-mode)))
+
+(add-hook 'c-ts-mode-hook
+     (lambda ()
+       (setq c-ts-mode-indent-offset 4
+             c-basic-offset 4
+             c-ts-mode-indent-style 'k&r)))
+
 
 ;;; Eglot (LSP client)
 (use-package eglot
@@ -363,8 +411,8 @@
      (eglot-events-buffer-size 0)   ;; disable noisy event buffer
      (eglot-confirm-server-initiated-edits nil)
      (eglot-extend-to-xref t) ;; reuse servers when jump to definition in library
-     :config
 
+     :config
      ;; clangd configuration
      (add-to-list 'eglot-server-programs
           '((c-ts-mode c++-ts-mode)
@@ -383,13 +431,10 @@
 
 (add-hook 'eglot-managed-mode-hook
      (lambda ()
-       (eglot-inlay-hints-mode -1)))
-
-(add-hook 'kill-buffer-hook
-     (lambda ()
-       (when (and (bound-and-true-p eglot-managed-mode)
-                  (not (eglot-current-server)))
-         (ignore-errors (eglot-shutdown)))))
+       (eglot-inlay-hints-mode -1))) ;; disable inlay hints by default
+                                        ; (add-hook 'kill-buffer-hook
+                                        ;      #'eglot-shutdown
+                                        ;      nil t)))
 
 ;;; Flymake diagnostics
 (use-package flymake
@@ -397,23 +442,61 @@
      :hook (eglot-managed-mode . flymake-mode)
      :custom
      (flymake-show-diagnostics-at-end-of-line nil) ;; no inline text
-     (flymake-indicator-type 'margins)
-     :bind (:map flymake-mode-map
-                 ("M-n" . flymake-goto-next-error)
-                 ("M-p" . flymake-goto-prev-error)
-                 ("C-c ! l" . flymake-show-buffer-diagnostics)))
+     (flymake-indicator-type 'margins))
+
+(defvar my/last-line -1
+  "Track the last line number we showed diagnostics for.")
+
+(defun my/flymake-show-line-diagnostics ()
+       "Show all Flymake diagnostics for the current line in the echo area."
+       (let ((line (line-number-at-pos)))
+         (unless (= line my/last-line)
+           (setq my/last-line line)
+           (let* ((beg (line-beginning-position))
+                  (end (line-end-position))
+                  (diags (flymake-diagnostics beg end)))
+             (when diags
+               (let ((msg (string-join
+                                  (mapcar #'flymake-diagnostic-text diags)
+                                  "\n")))
+                 (when (not (current-message))
+                   (message "%s" msg))))))))
+
+;; (add-hook 'flymake-mode-hook
+;;      (lambda ()
+;;             (add-hook 'post-command-hook
+;;                       #'my/flymake-show-line-diagnostics
+;;                       nil t)))
+
+
 (use-package eldoc
      :ensure nil
+     :config
+     (global-eldoc-mode 1)
      :custom
      (eldoc-echo-area-use-multiline-p nil)
      (eldoc-documentation-strategy
             'eldoc-documentation-compose)
-     :init
-     (global-eldoc-mode))
+     (eldoc-display-functions
+            '(eldoc-display-in-buffer)))
 
-;; Optional popup documentation
-;; (use-package eldoc-box
-;;   :hook (eglot-managed-mode . eldoc-box-hover-mode))
+(use-package eldoc-box
+     :after eglot
+     :custom
+     (eldoc-box-clear-with-C-g t))
+
+(defun my/eldoc-box-hover ()
+       (interactive)
+       (eldoc)
+       (run-with-idle-timer
+            0.05 nil #'eldoc-box-help-at-point))
+
+(add-hook 'eglot-managed-mode-hook
+     (lambda ()
+       (evil-local-set-key
+             'normal (kbd "K")
+             #'my/eldoc-box-hover)))
+
 
 ;;; Magit
 (use-package magit
@@ -442,14 +525,34 @@
         ("WARNING". "Red")
         ("FIX"    . "Gold")))
 
+(use-package markdown-mode)
+
+(use-package pdf-tools
+     :mode ("\\.pdf\\'" . pdf-view-mode)
+     :config
+     (pdf-tools-install)               ;; builds and enables
+     (setq-default pdf-view-display-size 'fit-page)
+     ;; faster continuous scroll
+     (setq pdf-view-continuous t
+           pdf-view-resize-factor 1.1)
+     ;; automatically revert when file changes
+     (add-hook 'pdf-view-mode-hook (lambda () (display-line-numbers-mode -1)))
+     (add-hook 'pdf-view-mode-hook 'auto-revert-mode))
+
+(add-hook 'emacs-startup-hook
+     (lambda ()
+       (setq gc-cons-threshold (* 100 1024 1024)
+             gc-cons-percentage 0.1)))
+
 ;; FIX:
-;; completion not perfect doesnt show all symbols
-;; enter in c-mode (atleast) gives two space after you write the line it indents properly (electric indent?)
+;; completion not perfect doesnt show all snippets
 ;; J to join line moves cursor (is it better worse? does it matter?)
-;; File and word completion works only in non-prog mode, cant even trigger it manually in say printf
-;; go mode just keeps starting servers for each file, and then servers just keep running after closing with 0 resource usage
-;; evil-numbers doesnt work
-;; python just starts ruff not basedpyright
 
 ;; NOTE: i think fixed
 ;; lsp server not stopping after all buffers closed
+;; enter in c-mode (atleast) gives two space after you write the line it indents properly (electric indent?)
+;; completion not perfect doesnt show all symbols
+;; evil-numbers doesnt work
+;; go mode just keeps starting servers for each file, and then servers just keep running after closing with 0 resource usage
+;; File and word completion works only in non-prog mode, cant even trigger it manually in say printf
+;; python just starts ruff not basedpyright
