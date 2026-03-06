9716cdd597e2a1811cddd303cffd2152f5289492
guptaanurag2106 <2106.guptaanurag@gmail.com>
2026-03-07 00:25:56 +0530

    Waybar minor changes

--- .config/scripts/cpu_temp.sh
+++ .config/scripts/cpu_temp.sh
@@ -5,6 +5,6 @@
 temp=$(sensors -j | jq '[.["coretemp-isa-0000"]["Core 0"]["temp2_input"], .["coretemp-isa-0000"]["Core 1"]["temp3_input"], .["coretemp-isa-0000"]["Core 2"]["temp4_input"], .["coretemp-isa-0000"]["Core 3"]["temp5_input"]] | add / length | (.*10 |round / 10)')
 
 # Output temperature in Waybar format
-echo "{\"text\": \" $temp°C\"}"
+echo "{\"text\": \"$temp°C\"}"
 # echo "{\"text\": \" 🌡️$temp°C\"}"
 
